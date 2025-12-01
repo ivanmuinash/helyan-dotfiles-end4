74937bd234698b9edc0f029e85217ed643e8ff2e
Helyan <ivanmuinash47@gmail.com>
2025-12-01 20:27:55 +0100

    feat: based btop theme config

---
+++ helyan.setup.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# btop
+rm -rf ~/.config/btop
+ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/btop ~/.config/btop
+
+# # fastfetch
+# rm -rf ~/.config/fastfetch
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/fastfetch ~/.config/fastfetch
+
+# # fish
+# rm -rf ~/.config/fish
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/fish ~/.config/fish
+
+# # fontconfig
+# rm -rf ~/.config/fontconfig
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/fontconfig ~/.config/fontconfig
+
+# # foot
+# rm -rf ~/.config/foot
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/foot ~/.config/foot
+
+# # fuzzel
+# rm -rf ~/.config/fuzzel
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/fuzzel ~/.config/fuzzel
+
+# # hypr
+# rm -rf ~/.config/hypr
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/hypr ~/.config/hypr
+
+# # illogical-impulse
+# rm -rf ~/.config/illogical-impulse
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/illogical-impulse ~/.config/illogical-impulse
+
+# # kde-material-you-colors
+# rm -rf ~/.config/kde-material-you-colors
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/kde-material-you-colors ~/.config/kde-material-you-colors
+
+# # kitty
+# rm -rf ~/.config/kitty
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/kitty ~/.config/kitty
+
+# # Kvantum
+# rm -rf ~/.config/Kvantum
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/Kvantum ~/.config/Kvantum
+
+# # matugen
+# rm -rf ~/.config/matugen
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/matugen ~/.config/matugen
+
+# # mpv
+# rm -rf ~/.config/mpv
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/mpv ~/.config/mpv
+
+# # quickshell
+# rm -rf ~/.config/quickshell
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/quickshell ~/.config/quickshell
+
+# # wlogout
+# rm -rf ~/.config/wlogout
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/wlogout ~/.config/wlogout
+
+# # xdg-desktop-portal
+# rm -rf ~/.config/xdg-desktop-portal
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/xdg-desktop-portal ~/.config/xdg-desktop-portal
+
+# # zshrc.d
+# rm -rf ~/.config/zshrc.d
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/zshrc.d ~/.config/zshrc.d
+
+# # file starship.toml
+# rm -f ~/.config/starship.toml
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/starship.toml ~/.config/starship.toml
+
+# # file chrome-flags.conf
+# rm -f ~/.config/chrome-flags.conf
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/chrome-flags.conf ~/.config/chrome-flags.conf
+
+# # file code-flags.conf
+# rm -f ~/.config/code-flags.conf
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/code-flags.conf ~/.config/code-flags.conf
+
+# # file darklyrc
+# rm -f ~/.config/darklyrc
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/darklyrc ~/.config/darklyrc
+
+# # file dolphinrc
+# rm -f ~/.config/dolphinrc
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/dolphinrc ~/.config/dolphinrc
+
+# # file kdeglobals
+# rm -f ~/.config/kdeglobals
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/kdeglobals ~/.config/kdeglobals
+
+# # file konsolerc
+# rm -f ~/.config/konsolerc
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/konsolerc ~/.config/konsolerc
+
+# # file thorium-flags.conf
+# rm -f ~/.config/thorium-flags.conf
+# ln -s ~/Documents/helyan-dotfiles-end4/dots/.config/thorium-flags.conf ~/.config/thorium-flags.conf
